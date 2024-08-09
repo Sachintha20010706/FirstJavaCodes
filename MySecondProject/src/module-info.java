cbde50d12425b70948a87780b578068df0633e9b
Sachintha20010706 <sachinthajayasuriya2001@gmail.com>
2024-08-09 14:38:51 +0530

    second project added

---
+++ MySecondProject/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MySecondProject {
+}
---
+++ MySecondProject/src/package1/FirstJavaProgram.java
@@ -0,0 +1,25 @@
+package package1;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+
+
+		int number = 10;
+		boolean car = true;
+		String President = "Richard Nixon";
+		char character = 'S';
+		long phonenumber = 7856498233333333345L;
+		double decimalvalue = 192.68D;
+		
+		System.out.println(number);
+		System.out.println(car);
+		System.out.println(President);
+		System.out.println(character);
+		System.out.println(phonenumber);
+		System.out.println(decimalvalue);
+
+	
+	}
+
+}
