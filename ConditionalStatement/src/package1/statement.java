7d3e82acdc955c81b4f70c389869d04a62e1b0dd
Sachintha20010706 <sachinthajayasuriya2001@gmail.com>
2024-08-12 11:03:26 +0530

    if-else commit

---
+++ ConditionalStatement/src/package1/statement.java
@@ -0,0 +1,28 @@
+package package1;
+
+public class statement {
+
+	public static void main(String[] args) {
+
+		int a = 100;
+		int b = 150;
+		
+		String city1 = "Newyork";
+		String city2 = "Las Vegas";
+		
+		/*System.out.println( a + b );
+		System.out.println(city1 + " " + city2);
+		
+		System.out.println( a + b + city1 + city2);
+		System.out.println( city1 + city2 + a + b);*/
+		
+		if(a<50) {
+			System.out.println(a);
+		}
+		
+		else{
+			System.out.println(b);
+		}
+	}
+
+}
